62dcd40bb49a2307c128403fe72d9f2c536005fa
Darshan Singh <dssinghm699@gmail.com>
2020-02-12 23:02:19 -0500

    Removed some print statements

--- mostTrafficked.py
+++ mostTrafficked.py
@@ -1,15 +1,35 @@
 import os
 import csv
 import numpy as np
-#from matplotlib import pyplot as plt
+
+####################
+# Global Variables #
+####################
 
 curr_path = os.getcwd()
-#LUT stands for LookUp Table
+
+#lut stands for LookUp Table
 code_lut = {}
 loc_lut = {}
 freq_lut = {}
 loc_freq_lut = {}
 
+####################
+# Helper functions #
+####################
+
+'''
+
+parse counts the number of charges that a pump has done in a month.
+
+    -called on every unique key in every months
+
+    -returns code_freq, or the frequency that the pump code that the function was
+        called on was used
+
+    -returned data is captured in the respective dictionary
+
+'''
 
 def parse(key, filepath):
     code_freq = 0
@@ -23,6 +43,17 @@ def parse(key, filepath):
     code_freq = code_freq // 5
     return code_freq
 
+###################
+# Start of script #
+###################
+
+'''
+
+    Here the file that stores all the pumps is parsed through to populate the
+        global dictionaries with keys that will be populated later.
+
+'''
+
 csv_file = open('Data\\socal_supercharger_posts_mapping.csv','r')
 locations = csv.reader(csv_file)
 next(locations)
@@ -35,11 +66,13 @@ for row in locations:
     loc_freq_lut.setdefault(temp_location_act, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     freq_lut[temp_code] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
+
+#Population of dictionaries occurs in this large nest of loops
+
 directory = os.fsencode((curr_path+"\\Data\\field_data"))
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
     for key in code_lut:
-        print("Parsing "+key + " in " + filename)
         code_frequency = parse(key, (curr_path+"\\Data\\field_data\\"+filename))
 
         if filename.startswith("Jan"):
@@ -47,88 +80,77 @@ for file in os.listdir(directory):
             loc_freq_lut[code_lut[key]][1] += code_frequency
             freq_lut[key][0] += code_frequency
             loc_freq_lut[code_lut[key]][0] += code_frequency
-            print("There were " + str(code_frequency) + " uses of pump " + key + " in the month of January." )
             continue
         elif filename.startswith("Feb"):
             freq_lut[key][2] = code_frequency
             loc_freq_lut[code_lut[key]][2] += code_frequency
             freq_lut[key][0] += code_frequency
             loc_freq_lut[code_lut[key]][0] += code_frequency
-            print("There were " + str(code_frequency) + " uses of pump " + key + " in the month of February." )
             continue
         elif filename.startswith("Mar"):
             freq_lut[key][3] = code_frequency
             loc_freq_lut[code_lut[key]][3] += code_frequency
             freq_lut[key][0] += code_frequency
             loc_freq_lut[code_lut[key]][0] += code_frequency
-            print("There were " + str(code_frequency) + " uses of pump " + key + " in the month of March." )
             continue
         elif filename.startswith("Apr"):
             freq_lut[key][4] = code_frequency
             loc_freq_lut[code_lut[key]][4] += code_frequency
             freq_lut[key][0] += code_frequency
             loc_freq_lut[code_lut[key]][0] += code_frequency
-            print("There were " + str(code_frequency) + " uses of pump " + key + " in the month of April." )
             continue
         elif filename.startswith("May"):
             freq_lut[key][5] = code_frequency
             loc_freq_lut[code_lut[key]][5] += code_frequency
             freq_lut[key][0] += code_frequency
             loc_freq_lut[code_lut[key]][0] += code_frequency
-            print("There were " + str(code_frequency) + " uses of pump " + key + " in the month of May." )
             continue
         elif filename.startswith("Jun"):
             freq_lut[key][6] = code_frequency
             loc_freq_lut[code_lut[key]][6] += code_frequency
             freq_lut[key][0] += code_frequency
             loc_freq_lut[code_lut[key]][0] += code_frequency
-            print("There were " + str(code_frequency) + " uses of pump " + key + " in the month of June." )
             continue
         elif filename.startswith("Jul"):
             freq_lut[key][7] = code_frequency
             loc_freq_lut[code_lut[key]][7] += code_frequency
             freq_lut[key][0] += code_frequency
             loc_freq_lut[code_lut[key]][0] += code_frequency
-            print("There were " + str(code_frequency) + " uses of pump " + key + " in the month of July." )
             continue
         elif filename.startswith("Aug"):
             freq_lut[key][8] = code_frequency
             loc_freq_lut[code_lut[key]][8] += code_frequency
             freq_lut[key][0] += code_frequency
             loc_freq_lut[code_lut[key]][0] += code_frequency
-            print("There were " + str(code_frequency) + " uses of pump " + key + " in the month of August." )
             continue
         elif filename.startswith("Sep"):
             freq_lut[key][9] = code_frequency
             loc_freq_lut[code_lut[key]][9] += code_frequency
             freq_lut[key][0] += code_frequency
             loc_freq_lut[code_lut[key]][0] += code_frequency
-            print("There were " + str(code_frequency) + " uses of pump " + key + " in the month of September." )
             continue
         elif filename.startswith("Oct"):
             freq_lut[key][10] = code_frequency
             loc_freq_lut[code_lut[key]][10] += code_frequency
             freq_lut[key][0] += code_frequency
             loc_freq_lut[code_lut[key]][0] += code_frequency
-            print("There were " + str(code_frequency) + " uses of pump " + key + " in the month of October." )
             continue
         elif filename.startswith("Nov"):
             freq_lut[key][11] = code_frequency
             loc_freq_lut[code_lut[key]][11] += code_frequency
             freq_lut[key][0] += code_frequency
             loc_freq_lut[code_lut[key]][0] += code_frequency
-            print("There were " + str(code_frequency) + " uses of pump " + key + " in the month of November." )
             continue
         elif filename.startswith("Dec"):
             freq_lut[key][12] = code_frequency
             loc_freq_lut[code_lut[key]][12] += code_frequency
             freq_lut[key][0] += code_frequency
             loc_freq_lut[code_lut[key]][0] += code_frequency
-            print("There were " + str(code_frequency) + " uses of pump " + key + " in the month of December." )
             continue
         else:
             continue
 
+#output of relevant data to the console
 
 months = ["2019","January","February","March","April","May","June","July","August","September","October","November","December"]
 
